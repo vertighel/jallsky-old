4b92510e47a90acf1aee7294af3eabeff0cd0890
vertighel <davide.ricci82@gmail.com>
2016-12-14 18:47:01 +0100

    Separating ws, mongo, serialport operations.

--- client.js
+++ client.js
@@ -51,7 +51,6 @@ $("#abort").on("click",function(event){
 /* opening a websocket connection to interact with other users.  Where is server.js? */
 var ws = new WebSocket('ws://localhost:1234', 'echo-protocol'); /// SET SAME PORT ON SERVER SIDE!
 //var ws = new WebSocket('ws://192.168.0.5:1234', 'echo-protocol'); /// SET SAME PORT ON SERVER SIDE!
-//var ws = new WebSocket('ws://192.168.1.23:1234', 'echo-protocol'); /// SET SAME PORT ON SERVER SIDE!
 //var ws = new WebSocket('ws://79.51.122.224:1234', 'echo-protocol'); /// SET SAME PORT ON SERVER SIDE!
 
 
